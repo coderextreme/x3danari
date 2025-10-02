7d6edc843afda240fd6eb5b44e4197d79d0c80f9
John W Carlson <yottzumm@gmail.com>
2025-10-02 07:42:47 -0500

    pretty good walking man

--- skinned_x3d_renderer.py
+++ skinned_x3d_renderer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49cc7ba15e919ea71913a513bf5a1b0a2d09cdcbdb47ab7838cfd203a959713f
-size 31415
+oid sha256:c2bd882669bf21cd2a1ef91f2e680dd3c238eeac6bfcaf34c21aae49cc0110f2
+size 31773
